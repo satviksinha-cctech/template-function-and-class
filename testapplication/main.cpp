32baa13e964a0a9f06c3bc305eca3b675644b7fd
Satvik Sinha <satvik.sinha@cctech.co.in>
2025-11-11 10:45:37 +0530

    UDT

--- testapplication/main.cpp
+++ testapplication/main.cpp
@@ -2,6 +2,23 @@
 #include <iostream>
 using namespace std;
 
+
+struct Item {
+    int id;
+    string name;
+
+    Item(int i, string n) : id(i), name(n) {}
+
+    bool operator==(const Item& other) const {
+        return id == other.id && name == other.name;
+    }
+
+    friend ostream& operator<<(ostream& os, const Item& item) {
+        os << "ID: " << item.id << ", Name: " << item.name;
+        return os;
+    }
+};
+
 int main() {
     cout << "Generic Inventory System "<<endl;
 
@@ -20,6 +37,15 @@ int main() {
     fruits.addItem("Oranges");
     fruits.removeItem("Bananas");
     fruits.display();
+
+    cout << endl;
+
+    Inventory<Item> storeItems;
+    storeItems.addItem(Item(101, "Laptop"));
+    storeItems.addItem(Item(102, "Mouse"));
+    storeItems.addItem(Item(103, "Keyboard"));
+    storeItems.removeItem(Item(102, "Mouse"));
+    storeItems.display();
 
     return 0;
 }
